73bafdde88fea65888e9cf271ca26be8abc6eae8
Kick Zurlohe <kickzurlohe@gmail.com>
2019-01-23 10:27:49 +0000

    minor js update

--- static/js/password.js
+++ static/js/password.js
@@ -1,5 +1,7 @@
 $(".toggle-password").click(function() {
 
+// https://itsolutionstuff.com/post/bootstrap-show-hidetoggle-password-input-field-using-bootstrap-show-passwordjs-pluginexample.html
+
   $(this).toggleClass("fa-eye fa-eye-slash");
   var input = $($(this).attr("toggle"));
   if (input.attr("type") == "password") {
