01de101348137bb523be944064fabb634452df7a
Ugurhan Candemir <ugurhancandemir@gmail.com>
2019-01-30 16:14:09 +0000

    Index werkt met like en al

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,12 +1,24 @@
 
 function like_photo(id){
-    $.ajax({
+    let request = $.ajax({
       url: "/like",
       type: "POST",
       data: {"id": id},
-      dataType: "text",
-
     });
+    request.done(update_photo_data);
+}
+
+function update_photo_data(data) {
+    var likeCount = document.getElementById("likecount");
+    likeCount.innerHTML = data.n_likes;
+    console.log(data.is_liked);
+    if (data.is_liked === true) {
+         document.getElementById("hart").classList.remove('black');
+         console.log(data.n_likes)
+    } else {
+         document.getElementById("hart").classList.add('black');
+         console.log(data.n_likes)
+    }
 }
 
 
