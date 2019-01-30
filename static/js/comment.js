8201115ffcc3eb7075a37c0bc7c10522db6f6146
Ugurhan Candemir <ugurhancandemir@gmail.com>
2019-01-30 19:14:07 +0000

    added comments

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,45 +1,20 @@
-// Initial Search Buttons
-var topics = ["Internet Cats", "Meme's", "Typing", "Space", "Rick and Morty"];
-function addSearchBtns() {
-  $("#buttons").html("");
-  for (i = 0; i < topics.length; i++) {
-    var $button = $("<input type='button' class='btn btn-sm search-btn' />");
-    $button.val(topics[i]);
-    $("#buttons").append($button);
-  }
-}
-addSearchBtns();
 $( document ).ready(function() {
-    console.log("ready");
-
-    $("#search-txt").on("input", function() {
-        console.log("change");
-
-    })
-   $("button").on("click", function() {
-       console.log("searching");
-   })
+  // select an image by putting a border around it and remembering the id when clicked
   $(document).on("click","#results img",function(){
-	$(this).css("border","5px solid green");
-	$("#results img").not($(this)).css("border","none");
-	cm_url = this.src
-	//console.log(link)
-
+    	$(this).css("border","5px solid green");
+    	$("#results img").not($(this)).css("border","none");
+    	cm_url = this.src
 });
 
 $('.myImg').click(function(){
     imgid = this.id
 });
 
-
+// send the id of the image and the url of the gif for submission
 $('#comments').click(function() {
-        console.log(cm_url);
         $.ajax({
           type: "POST",
-          //contentType: "application/json;charset=utf-8",
           url: "/comment",
-          //traditional: "true",
-          //data: JSON.stringify({cm_url}),
           data: {"cm_url": cm_url, "id": imgid},
           dataType: "text",
           });
@@ -48,42 +23,21 @@ $('#comments').click(function() {
 $('#giphy').hide();
 $("#giphy .section").hide();
 
-
-if ($('#element').is(':empty')){
-  //do something
-} else{
-  console.log("found")
-}
-
-/*
-$("#Showbutton").click(function(e){
-     $('#giphy').toggle('slow');//or just show instead of toggle
-}); */
+// show the gif lookup section
 $('#Showbutton').on("click", function () {
         $('#giphy').slideToggle("slow", function () {
             $(window).scrollTop($(this).offset().top + $(this).height());
         });
     });
 
-
-
+// show the requested gifs when the user types and hide them when the user stops typing
 $('#user-search').on("keyup", function () {
   var chatinput = document.getElementById("user-search").value;
     if (chatinput == "" || chatinput.length == 0 || chatinput == null)
     {
-        // Invalid... Box is empty
-        console.log("empty")
         $("#giphy .section").hide();
-
-
-
-
     } else {
-      console.log("yay")
-
       $("#giphy .section").show();
-
-
     }
 });
 
@@ -93,7 +47,7 @@ $('#user-search').on("keyup", function () {
 
 
 
-
+// when user stops typing request the gifs
 $(document).on("keyup", "#user-search", function giphy() {
 
   $('#user-search').keyup(delay(function (e) {
@@ -105,7 +59,6 @@ $(document).on("keyup", "#user-search", function giphy() {
   var params = {
     q: query,
     limit: 10,
-    //api_key: "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
     api_key: "acWYyORZmqoAwhmQACttO8SL7VGVV0Mw",
     fmt: "json"
   };
@@ -113,11 +66,6 @@ $(document).on("keyup", "#user-search", function giphy() {
     query = $(this).val();
   } else if ($("#user-search").val() !== "") {
     query = $("#user-search").val();
-    topics.push(query);
-    if (topics.length > 6) {
-      topics.shift();
-    }
-    addSearchBtns();
   }
   params.q = query;
 
@@ -132,57 +80,18 @@ $(document).on("keyup", "#user-search", function giphy() {
       for (i = 0; i < params.limit; i++) {
         var $img = $("<img>");
         var $div = $("<div>");
-        //var $rating = $("<h6>");
         var gifObj = r.data[i];
         var gif = gifObj.images;
 
         // Image builder object
         $img.attr({
-           //"width": "50px",
-          //src: gif.fixed_height_still.url,
           src: gif.fixed_height.url,
-          //"data-animate": gif.fixed_height.url,
-          //"data-still": gif.fixed_height_still.url,
-          //"data-state": "still",
           class: "gif"
         });
-        // $div.attr("id", "gif-" + i);
         $div.addClass("gif-box");
-       // $rating.text("Rating: " + gifObj.rating);
         $div.append($img);//, $rating);
         $("#results").append($div);
       }
-/*
-      $(".gif").on("click", function() {
-        var link = this.src
-        console.log(link)
-        if (!this.style.border) {
-        this.style.border = "3px solid blue";
-        }
-        else {git ad
-            this.style.border = '';
-        }
-      });
-        */
-
-
-
-
-
-
-
-
-
-        /*
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-        */
 
     }
   });
@@ -192,7 +101,7 @@ $(document).on("keyup", "#user-search", function giphy() {
 
 
 });
-
+// timer between user-input and api request
 function delay(callback, ms) {
   var timer = 0;
   return function() {
--- static/js/index.js
+++ static/js/index.js
@@ -6,12 +6,13 @@ var modal = document.getElementById('myModal');
 var img = $('.myImg');
 var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-
+// display the images in a modal on click
 $('.myImg').click(function(){
     modal.style.display = "block";
     var newSrc = this.src;
     modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
+    // send the id of the photo to request the comments
     $.ajax({
           url: "/zien_comments",
           type: "POST",
@@ -34,27 +35,20 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
-$('#like').click(function(){
-    console.log("likeclick")
-});
 
-
-
-
-
+// hide the comment section and gif lookup section when the picture is closed
 $('#del').click(function(){
     $("#cmbx img").remove();
     $('#cmbx').hide()
     $('#giphy').hide()
 });
 $('#cmbx').hide()
+
+// get the links from the comments posted on this picture
 $('#scomments').click(function(){
     $('#cmbx').toggle();
-    //$('#cmbox').html('')
     $('#cmbx img').remove();
     $("#giphy .section").hide();
-
-    console.log($('#cmbox').html())
     $.ajax({
         type: "GET",
                 url: '/zien_comments',
@@ -64,27 +58,20 @@ $('#scomments').click(function(){
                     for (i = 0; i < cmlist.length; i++) {
                     var elem = document.createElement("img");
                     elem.setAttribute("src", cmlist[i]);
-                    console.log(elem.src)
-
                     document.getElementById("cmbx").appendChild(elem);
-
-
-
-
                 }
             }
           });
 });
 
+// reset the gif lookup section on comment submission
 $('#comments').click(function(){
     $('#cmbx').show('slow')
     $('#cmbox').html('')
-    //$('#cmbox img').remove()
     $('#giphy').hide('slow')
     $('#cmbx img').remove()
     $("#results img").remove();
     $("#results div").remove();
-    console.log($('#cmbox').html())
     $.ajax({
                 type: "GET",
                 url: '/zien_comments',
@@ -94,18 +81,12 @@ $('#comments').click(function(){
                     for (i = 0; i < cmlist.length; i++) {
                     var elem = document.createElement("img");
                     elem.setAttribute("src", cmlist[i]);
-                    console.log(elem.src)
 
                     document.getElementById("cmbx").appendChild(elem);
-
-
-
-
             }
             }
           });
           document.getElementById('user-search').value = "";
-          console.log($('#cmbox').html())
 });
 
 $(document).on('click','#hart', function(event) {
