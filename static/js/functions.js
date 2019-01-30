9578af8a12964d1c7aba4bf880e614c053c53685
Ugurhan Candemir <ugurhancandemir@gmail.com>
2019-01-30 19:19:38 +0000

    unnodige files deleted and some more comments

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,4 +1,4 @@
-
+// send the photo id
 function like_photo(id){
     let request = $.ajax({
       url: "/like",
@@ -7,35 +7,13 @@ function like_photo(id){
     });
     request.done(update_photo_data);
 }
-
+// check if photo is liked by user and how many likes the photo has
 function update_photo_data(data) {
     var likeCount = document.getElementById("likecount");
     likeCount.innerHTML = data.n_likes;
-    console.log(data.is_liked);
     if (data.is_liked === true) {
          document.getElementById("hart").classList.remove('black');
-         console.log(data.n_likes)
     } else {
          document.getElementById("hart").classList.add('black');
-         console.log(data.n_likes)
     }
-}
-
-
-function change(){
-    var elem = document.getElementById("likebutton");
-    if (elem.value=="liked") elem.value = "unliked";
-    else elem.value = "liked";
-}
-
-
-
-function lcm(id){
-    $.ajax({
-      url: "/",
-      type: "POST",
-      data: {"id": id},
-      dataType: "text",
-
-    });
 }
